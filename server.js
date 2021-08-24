3b7738eb76f2fde0ca9cd336bd75039ce2b6d498
James Dobbs <dobbs282@gmail.com>
2021-08-24 16:39:36 -0400

    step back to fix heroku

--- server.js
+++ server.js
@@ -1,29 +1,14 @@
 const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
-const session = require("express-session");
-const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
-const helpers = require("./utils/helper");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-  secret: process.env.SESSION_PASSWORD,
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
-
-app.use(session(sess));
-
-const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create({});
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
