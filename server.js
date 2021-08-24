c6d0d3e73ded41581a8381da98c9c64b8531e19e
James Dobbs <dobbs282@gmail.com>
2021-08-24 16:33:00 -0400

    fix heroku deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const express = require("express");
-const session = require("express-session");
 const exphbs = require("express-handlebars");
+const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const routes = require("./controllers");
