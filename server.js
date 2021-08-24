5b58c27b0e2f9c18576ac14d2d3a590fbbe15759
James Dobbs <dobbs282@gmail.com>
2021-08-24 16:25:46 -0400

    added sessions

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const path = require("path");
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
+const helpers = require("./utils/helper");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const sess = {
+  secret: process.env.SESSION_PASSWORD,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
