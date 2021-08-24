f4515fda68754d4c4d9abea634f4ee43ab102cdd
James Dobbs <dobbs282@gmail.com>
2021-08-24 17:42:02 -0400

    brought in handlebars with default pages

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,27 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
-  res.send("Home");
+  res.render("home");
+});
+
+router.get("/comment", (req, res) => {
+  res.render("comment");
+});
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard");
+});
+
+router.get("/post", (req, res) => {
+  res.render("post");
+});
+
+router.get("/updatePost", (req, res) => {
+  res.render("updatePost");
+});
+
+router.get("/login", (req, res) => {
+  res.render("login");
 });
 
 module.exports = router;
